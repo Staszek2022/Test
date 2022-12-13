08f1d52c86b7db0787aa939fa3ca94736810ed2a
Staszek2022 <stanislaw.szad@gmail.com>
2022-12-13 18:27:47 +0100

    Rozmiar okna

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from tkinter import *
+import threading
+import time
+
+def okno():
+    for i in range(50):
+        time.sleep(0.1)
+        print(window.winfo_geometry())
 
 window = Tk()
+window.geometry("429x557")
+threading.Thread(target=okno).start()
 window.mainloop()
 
